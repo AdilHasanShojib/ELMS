d595001564ba826d87284bdd742d96efe6d33236
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-10-27 22:38:18 +0600

    Authentication Part 1

---
+++ SocialAPI/TDto/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SocialAPI.TDto
+{
+    public class LoginDto
+    {
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+
+
+
+    }
+}
---
+++ SocialAPI/TDto/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialAPI.TDto
+{
+    public class RegisterDto
+    {
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4)]
+        public string Password { get; set; }
+
+
+
+
+    }
+}
