60437879f40f75dd87f06b0c08f3a9b4186cef90
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-10-27 21:41:05 +0600

    Project Skeleton Part 1 & part 2

---
+++ SocialAPI/Program.cs
@@ -0,0 +1,48 @@
+
+using Microsoft.EntityFrameworkCore;
+using SocialAPI.TData;
+
+namespace SocialAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddCors();
+            builder.Services.AddHttpContextAccessor();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddDbContext<TDataContext>(opt =>
+            {
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("LearnSocialApp"));
+
+            });
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            //app.UseHttpsRedirection();
+
+            //app.UseAuthorization();
+
+            app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:4200"));
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
